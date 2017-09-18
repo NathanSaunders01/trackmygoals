14999617845d67c29fe870ae15bb23fea20d6322
Nathan Saunders <nathan_saunders@hotmail.co.uk>
2017-09-18 15:37:14 +0000

    Home page styling and navbar

--- trackmygoals/config/routes.rb
+++ trackmygoals/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  root to: 'pages#hpome'
+  root to: 'pages#home'
   
   get 'about', to: 'pages#about'
 end

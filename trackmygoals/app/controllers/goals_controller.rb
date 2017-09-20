512a22434418a9b4576530597ad73315f87efae5
Nathan Saunders <nathan_saunders@hotmail.co.uk>
2017-09-20 15:55:22 +0000

    Add more to goals controller and styling

--- trackmygoals/app/controllers/goals_controller.rb
+++ trackmygoals/app/controllers/goals_controller.rb
@@ -1,4 +1,7 @@
 class GoalsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_goal, only: [:edit, :update, :show, :destroy]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
   
   def new
     @goal = Goal.new
@@ -20,21 +23,43 @@ class GoalsController < ApplicationController
   end
   
   def update
+    if @goal.update(goal_params)
+      flash[:success] = "Goal was successfully updated!"
+      redirect_to goal_path(@goal)
+    else
+      flash[:danger] = @goal.errors.full_messages.join(", ")
+      render 'edit'
+    end
   end
   
   def show
-    @goal = Goal.find_by_id(params[:id])
   end
   
   def index
+    @user = current_user
+    @user_goals = @user.goals
   end
   
   def destroy
+    @goal.destroy
+    flash[:danger] = "Goal was successfully deleted!"
+    redirect_to goals_path
   end
   
   private
   
   def goal_params
     params.require(:goal).permit(:name, :description, :xp_value)
+  end
+  
+  def set_goal
+    @goal = Goal.find(params[:id])
+  end
+  
+  def require_same_user
+    if @goal.user != current_user
+      flash[:danger] = "You can only access your own goals."
+      redirect_to root_path
+    end
   end
 end

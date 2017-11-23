50ea74245b92fc05fd3f33d4ffe401c95b162d67
Nathan Saunders <nathan_saunders@hotmail.co.uk>
2017-11-23 15:50:28 +0000

    Added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,10 +13,10 @@
 #   Activity.create!(user_id: 5, goal_id: 31, quantity: "#{a}".to_i, total_xp: 100*"#{a}".to_i, created_at: a.days.ago)
 # end
 
-5.times do |a|
-  Activity.create!(user_id: 1, goal_id: 2, quantity: 1, total_xp: 500, created_at: (a+1).weeks.ago)
-end
+# 5.times do |a|
+#   Activity.create!(user_id: 1, goal_id: 2, quantity: 1, total_xp: 500, created_at: (a+1).weeks.ago)
+# end
 
 3.times do |a|
-  Activity.create!(user_id: 1, goal_id: 1, quantity: a.to_i, total_xp: 250, created_at: (a-1).days.ago)
+  Activity.create!(user_id: 1, goal_id: 1, quantity: a.to_i, total_xp: 250, created_at: (a+2).days.ago)
 end
